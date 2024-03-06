91fe6e8ae219508c068d192d50d451cd4d7a8c7a
Ana julia <ana-julia_f_santos@estudante.sesisenai.org.br>
2024-03-06 15:34:02 -0300

    soma de arrays no back and

---
+++ principal_array.js
@@ -0,0 +1,62 @@
+const readlineSync = require('readline-sync')
+
+let numArray1 = [1,2,3,4,5]
+let numArray2 = []
+let numArray3 = []
+ 
+
+let valor = 0
+
+function soma(){
+    for(let i=0;i<=4;i++){
+        numArray3[i] = numArray1[i] + numArray2[i]
+
+
+}
+return numArray3
+
+function ler (){
+    let contador = 0 
+    while(contador <= 4){
+        valor = Number(readlineSync.question('Digite um valor para ser guardado: '))
+        numArray2.push(valor)
+        contador += 1
+       
+    
+}
+return numArray3
+
+}
+
+
+
+console.log('======================================================')
+console.log('Soma dos valores de 2 arrays')
+console.log('======================================================')
+
+let leia = ler ()
+let resposta = soma ()
+
+let contador = 0 
+while(contador <= 4){
+ valor = Number(readlineSync.question('Digite um valor para ser guardado: '))
+ numArray2.push(valor)
+ contador += 1
+
+}
+
+  return numArray2
+
+console.log('O Array 2 é = ',numArray2)
+
+
+//teste 1
+let resposta = soma ()
+console.log(resposta)
+
+
+//teste2
+//soma()
+}
+
+// console.log('O array 3 é =',numArray2)
---
+++ teste.js
@@ -0,0 +1,21 @@
+const readlineSync = require('readline-sync')
+
+let numArray1 = [1,2,3,4,5]
+let numArray2 = []
+let numArray3 = []
+ 
+
+let valor = 0
+
+function subtrair(){
+
+}
+
+function ler(){
+
+
+}
+
+function imprimir(){
+    
+}
